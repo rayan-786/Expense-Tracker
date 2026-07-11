16d3bb47829e61d879dd2920d3401ac20c179239
Rayan Ahmad <ansarirayan86@gmail.com>
2026-07-11 18:47:40 -0400

    updated

--- Expens/knexfile.cjs
+++ Expens/knexfile.cjs
@@ -6,17 +6,15 @@ module.exports = {
     client: "mysql2",
 
     connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT || 3306,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    },
-
-    migrations: {
-      directory: path.resolve(__dirname, "migrations"),
-      tableName: "knex_migrations"
-    },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 4000,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
+},
 
     seeds: {
       directory: path.resolve(__dirname, "seeds"),
