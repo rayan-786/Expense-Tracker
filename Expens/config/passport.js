5ad6f3eb14f03c451296a7a95b4b493d0cc7519c
Rayan Ahmad <ansarirayan86@gmail.com>
2026-08-13 14:06:52 +0530

    ENV SET

--- Expens/config/passport.js
+++ Expens/config/passport.js
@@ -1,25 +1,20 @@
-const passport = require("passport");
-const GitHubStrategy = require("passport-github2").Strategy;
-
 passport.use(
   new GitHubStrategy(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+
       callbackURL:
-    `${process.env.BACKEND_URL}/auth/github/callback`,
+        `${process.env.BACKEND_URL}/api/auth/github/callback`,
+
       scope: ["user:email"],
     },
 
     async (accessToken, refreshToken, profile, done) => {
       try {
-
         let email = null;
 
-        if (
-          profile.emails &&
-          profile.emails.length > 0
-        ) {
+        if (profile.emails && profile.emails.length > 0) {
           email = profile.emails[0].value;
         }
 
@@ -35,5 +30,3 @@ passport.use(
     }
   )
 );
-
-module.exports = passport;
